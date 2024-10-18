93e5d3d9fc9bac032c9e6d55ba5a3284cc45f1fa
molchanovz <molchanov.top@gmail.com>
2024-10-18 13:19:37 +0300

    edit inlineButtons handler

--- pkg/app/botHandlers.go
+++ pkg/app/botHandlers.go
@@ -37,7 +37,7 @@ func (a *App) handleInfo(ctx context.Context, b *bot.Bot, update *models.Update)
 	userId, _ := strconv.Atoi(userIdStr)
 	fmt.Printf("Наш юзер %d", userId)
 	c := sales.NewDefaultClient("http://91.222.239.37:8080/v1/rpc/")
-	info, err := c.Sales.GetMyInfo(ctx)
+	info, err := c.Sales.SendTextMessageByTgChatID(ctx, userId, "Ответное сообщение")
 	if err != nil {
 		fmt.Println(err)
 		return
